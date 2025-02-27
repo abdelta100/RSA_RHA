754f3fe14348200103f455134e18f9b14781739a
Blabadooshk <70954777+abdelta100@users.noreply.github.com.>
2025-02-26 18:42:25 -0600

    Some amendment to duhamel but not checked yet

--- Integrators/Duhamel.py
+++ Integrators/Duhamel.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.ma.core import concatenate
 
 
+
 def duhamel(ground_accel_arr, t_arr: np.ndarray, wn: float = 6, zeta: float = 0.1, m: float = 1) -> [np.ndarray,
                                                                                                      np.ndarray,
                                                                                                      np.ndarray]:
@@ -11,17 +12,20 @@ def duhamel(ground_accel_arr, t_arr: np.ndarray, wn: float = 6, zeta: float = 0.
 
     # If timesteps are equal
     tstep = t_arr[1] - t_arr[0]
-
+    z_max=max(abs(ground_accel_arr))
     wd = wn*np.sqrt(1 - zeta ** 2)
+    print(z_max)
     # impulse_resp_func=1/(m*wd)*np.exp(-zeta*wn*t_arr)*np.sin(wd*t_arr)
-    impulse_resp_func = 1 / (wd) * np.exp(-zeta * wn * t_arr) * np.sin(wd * t_arr)
+    impulse_resp_func = (1 / (wd*z_max)) * np.exp(-zeta * wn * t_arr) * np.sin(wd * t_arr)
 
     # disp = np.convolve(ground_accel_arr, impulse_resp_func, mode='same')
-    disp = np.convolve(impulse_resp_func,ground_accel_arr, mode='same')
-    vel = disp[0:-1] - disp[1:] / tstep
-    vel = concatenate(([0], vel))
+    disp = np.convolve(impulse_resp_func,ground_accel_arr, mode='full')
+    disp=disp[0:len(t_arr)]*tstep
 
-    accel = (vel[0:-1] - vel[1:]) / tstep
-    accel = concatenate(([0], accel))
+    vel = (disp[1:] - disp[:-1]) / tstep
+    vel = np.concatenate(([0], vel))
+
+    accel = (vel[1:] - vel[:-1]) / tstep
+    accel = np.concatenate(([0], accel))
 
     return disp, vel, accel
