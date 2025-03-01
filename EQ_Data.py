9ddfa9042272931a597cd22dbdd01af2a666d2be
Blabadooshk <70954777+abdelta100@users.noreply.github.com.>
2025-03-01 11:16:44 -0600

    included metedata in earthquake class

--- EQ_Data.py
+++ EQ_Data.py
@@ -1,32 +1,50 @@
 import numpy as np
-from Constants import g
+from Constants import *
+
+
 class EQ_Data():
 
-    def __init__(self, time, ground_accel, g_units=True):
+    def __init__(self, time, ground_accel, units='g', name="Unknown", orientation="unknown", location="Unknown"):
 
-        self.name="Unknown"
+        self.name = name
+        self.orientation=orientation
+        self.location=location
+
+        self.meta_name = name+ " - " + orientation + " - " + location
         self.g = g
-        self.is_orig_unit_g = g_units
+        self.orig_units = units
 
         self.getGroundAccelInUnits(ground_accel)
-        self.time=time
+        self.time = time
         # assume constant timestep for now
-        self.dt=time[1]-time[0]
-
+        self.dt = time[1] - time[0]
 
     def getGroundAccelInUnits(self, ground_accel):
-        if self.is_orig_unit_g:
-            self.ground_accel=ground_accel * self. g
-            self.ground_accel_g=ground_accel
-        else:
-            self.ground_accel=ground_accel
-            self.ground_accel_g=ground_accel/self.g
+        if self.orig_units == "SI":
+            self.ground_accel = ground_accel
+            self.ground_accel_g = ground_accel/g_SI
+        elif self.orig_units=="Imp":
+            self.ground_accel = ground_accel
+            self.ground_accel_g = ground_accel/g_Imp
+        elif self.orig_units == "g":
+            # TODO add working units condition here
+            self.ground_accel = ground_accel*g_SI
+            self.ground_accel_g = ground_accel
 
+    @property
+    def ground_accel_working(self, working_units: str="SI"):
+        if working_units=="SI":
+            return self.ground_accel_g * g_SI
+        elif working_units=="Imp":
+            return self.ground_accel_g * g_Imp
+        else:
+            print("Invalid Units")
+            return 0
 
 class Sin_EQ(EQ_Data):
 
     def __init__(self, amplitude=1, frequency=3, t_end=10, t_step=0.1):
-        t=np.arange(0, t_end,t_step )
-        ground_accel=amplitude*np.sin(frequency*t)
-        super().__init__(t, ground_accel, g_units=False)
+        t = np.arange(0, t_end, t_step)
+        ground_accel = amplitude * np.sin(frequency * t)
+        super().__init__(t, ground_accel, units="Imp", name="Sin Wave")
         # self.ground_accel
