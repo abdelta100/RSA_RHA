096355b1cb00cb0966f3e75f61d114360a2fc20b
Blabadooshk <70954777+abdelta100@users.noreply.github.com.>
2025-03-02 13:37:08 -0600

    Added pseudo spectrum calculator

--- EQ_Aggregator_Scripts/Spectral_Values_Aggregate.py
+++ EQ_Aggregator_Scripts/Spectral_Values_Aggregate.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from EQ_Data import EQ_Data
 from Response_History.SpectralValues import getFullSpectralDVA_T_range
+from Response_Spectrum.PsuedoSpectralValues import getFullSpectralDPVPA_T_range
 
 
 def getFullSpectralDVA_Aggregate(eq_list=list[EQ_Data], T=None, step=0.02, zeta=0.15):
@@ -22,3 +23,20 @@ def getFullSpectralDVA_Aggregate(eq_list=list[EQ_Data], T=None, step=0.02, zeta=
         print("Finished "+ eq.meta_name)
 
     return D, V, A, T
+
+def getFullSpectralDPVPA_Aggregate(eq_list=list[EQ_Data], T=None, step=0.02, zeta=0.15):
+    if isinstance(T, NoneType):
+        T = np.arange(0.05, 20, step=step)
+
+    D = np.zeros((len(T), len(zeta), len(eq_list)))
+    PV = np.zeros((len(T), len(zeta), len(eq_list)))
+    PA = np.zeros((len(T), len(zeta), len(eq_list)))
+
+    for i, eq in enumerate(eq_list):
+        d, v, a, _ = getFullSpectralDPVPA_T_range(eq, T, step, zeta)
+        D[:, :, i] = d
+        PV[:, :, i] = v
+        PA[:, :, i] = a
+        print("Finished "+ eq.meta_name)
+
+    return D, PV, PA, T
