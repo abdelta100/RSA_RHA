76984aff58d77966d23523590f80c21ee4629e8a
Blabadooshk <70954777+abdelta100@users.noreply.github.com.>
2025-03-06 12:37:53 -0600

    Major immprovements

--- EQ_Aggregator_Scripts/Tripartite__Plot.py
+++ EQ_Aggregator_Scripts/Tripartite__Plot.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, FuncFormatter
+
+from Constants import *
 
 
 # https://github.com/anismhd/TRIPARTITE/blob/master/tripartite.py
 
-def tripartite_axis(axis):
+def tripartite_axis(axis, name):
+    plt.rcParams['font.family'] = 'serif'
     axis.set_xscale('log')
     axis.set_yscale('log')
     axis.set_aspect('equal')
@@ -12,11 +16,27 @@ def tripartite_axis(axis):
     axis.set_xlim(xlim)
     ylim = axis.get_ylim()
     axis.set_ylim(ylim)
+
+    axis.xaxis.set_major_formatter(FuncFormatter(lambda val, pos: format_number(val)))
+    axis.yaxis.set_major_formatter(FuncFormatter(lambda val, pos: format_number(val)))
+
+    # Optional: To make sure all ticks are shown as numbers
+    # axis.ticklabel_format(style='plain', axis='both')
+
+
     tick_font_size = axis.get_xticklabels()[0].get_fontsize()
     # TODO use a label keyword
     axis.grid(True, which='both', color='k', linestyle='-', alpha=.3, lw=.3)
     axis.set_xlabel('Period (sec.)')
     axis.set_ylabel('Spectral Velocity')
+    axis.set_title(name)
+    a_c = 2 * np.pi / g_list[working_units]
+    d_c = 1 / (2 * np.pi)
+
+    # acc_axis
+    axis.plot(xlim, 10 ** (1 / (2 * np.pi)) / np.array(xlim), c='k', lw=1)
+    # # d_axis
+    axis.plot(xlim, 10 ** (2 * np.pi / g_list[working_units]) * np.array(xlim) / 2 * np.pi, c='k', lw=1)
 
     # Finding the middle point of the graph
     xcenter = 10 ** (0.5 * sum(np.log10(xlim)))
@@ -35,47 +55,92 @@ def tripartite_axis(axis):
     bbox = {'fc': '0.8', 'pad': 0}
 
     for accl in range(int(log10ACCL_min), int(log10ACCL_max) + 1):
-        axis.plot(xlim, 10 ** accl * np.array(xlim) / (2 * np.pi), c='k', lw=.5, alpha=.3)
-        loc_x = -0.5 * (accl - np.log10(2 * np.pi) - C2)
-        loc_y = -loc_x + C2
-        axis.text(10 ** loc_x, 10 ** loc_y, "{:.1g}".format(10 ** accl),
-                  ha='center', va='center', rotation=45, color='k', fontsize=tick_font_size,
-                  bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'))
+        # formatted_number = r"$10^{" + f"{accl}" + r"}$"
+        formatted_number = format_number(10 ** accl)
+        axis.plot(xlim, 10 ** accl * np.array(xlim) / 2 * np.pi, c='k', lw=.7, alpha=.3)
+
+        l_a = accl + 1
+        l_d = -2 * np.pi / g_list[working_units]
+        loc_v = np.sqrt(10 ** (l_d) * 10 ** (l_a))
+
+        loc_t = 2 * np.pi * 10 ** (l_d) / loc_v
+
+        if (loc_v >= ylim[0]) and (loc_v <= ylim[1]) and (loc_t >= xlim[0]) and (loc_t <= xlim[1]):
+            axis.text(loc_t, loc_v, formatted_number,
+                    ha='center', va='center', rotation=45, color='k', fontsize=tick_font_size,
+                    bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'), family='Times New Roman')
 
         for i in range(1, 10):
-            axis.plot(xlim, 10 ** accl * i * np.array(xlim) / (2 * np.pi), c='k', lw=.5, alpha=.3)
+            axis.plot(xlim, 10 ** accl * i * np.array(xlim) / 2 * np.pi, c='k', lw=.5, alpha=.3)
 
     for disp in range(int(log10DISP_min), int(log10DISP_max) + 1):
-        formatted_number = r"$10^{" + f"{disp}" + r"}$"  # LaTeX style for power of 10
+        # formatted_number = r"$10^{" + f"{disp}" + r"}$"  # LaTeX style for power of 10
+        formatted_number=format_number(10**disp)
         axis.plot(xlim, 10 ** disp * 2 * np.pi / np.array(xlim), c='k', lw=.5, alpha=.3)
-        loc_x = 0.5 * (disp + np.log10(2 * np.pi) - C1)
-        loc_y = loc_x + C1
-        axis.text(10 ** loc_x, 10 ** loc_y, formatted_number,
-                  horizontalalignment='center', rotation=-45,
-                  verticalalignment='center', color='k', fontsize=tick_font_size,
-                  bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'))
+
+        offset_a = 0.1
+        l_a = (1 / (2 * np.pi)) * g_list[working_units] + offset_a
+        l_d = disp
+        loc_v = np.sqrt(10 ** (l_d) * 10 ** (l_a))
+
+        loc_t = 2 * np.pi * 10 ** (l_d) / loc_v
+        if (loc_v >= ylim[0]) and (loc_v <= ylim[1]) and (loc_t >= xlim[0]) and (loc_t <= xlim[1]):
+            axis.text(loc_t, loc_v, formatted_number,
+                     horizontalalignment='center', rotation=-45,
+                    verticalalignment='center', color='k', fontsize=tick_font_size,
+                    bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'), family='Times New Roman')
+
 
         for i in range(1, 10):
             axis.plot(xlim, 10 ** (disp) * i * 2 * np.pi / np.array(xlim), c='k', lw=.5, alpha=.3)
 
-    axis.plot(xlim, 10 ** (log10DISP_mid) * 2 * np.pi / np.array(xlim), c='k', lw=1)
-    axis.plot(xlim, 10 ** (log10ACCL_mid) * np.array(xlim) / (2 * np.pi), c='k', lw=1)
     # axis.plot([10 ** loc_x, 5], [10 ** loc_y, 5], color='k', linewidth=2)
     return axis
 
 
-def tripartitePlot(D, V, A, T, save=False, filename="tripartite_plot.png"):
+def tripartitePlot(D, V, A, T, name="Tri-Log Plot"):
+    plt.rcParams['font.family'] = 'serif'
     fig = plt.figure()
+    fig.set_size_inches(6.5, 5)
     ax = plt.gca()
     ax.plot(T, V, c='k')
-    ax = tripartite_axis(ax)
-    plt.show()
-
-    if save:
-        plt.savefig(filename)
-    plt.show()
+    ax = tripartite_axis(ax, name=name)
+    # plt.show()
+    #
+    # if save:
+    #     plt.savefig(filename)
+    # plt.show()
     return fig
 
+def tripartitePlotSeries(D, V, A, T, name="Tri-Log Plot", legend_list=None):
+    plt.rcParams['font.family'] = 'serif'
+    fig = plt.figure()
+    fig.set_size_inches(6.5,5)
+    ax = plt.gca()
+    # ax.plot(T, V, c='k')
+    for i in range(V.shape[2]):
+        ax.plot(T,V[:,0,i],label=legend_list[i])
+    # plt.show()
+    ax.legend()
+    ax = tripartite_axis(ax, name=name)
+    #
+    # if save:
+    #     plt.savefig(filename)
+    # plt.show()
+    return fig
+
+
+def format_number(num):
+    # If the number is very close to an integer (within a small tolerance), treat it as an integer
+    if num.is_integer():
+        return f"{int(num)}"  # No decimals for whole numbers
+    else:
+        # Otherwise, show the number with significant decimals
+        return f"{num:.10g}"  # Adjust precision for significant digits (up to 10 significant digits)
+
+# Example values
+accl = 0.00123  # Example acceleration value
+formatted_number = format_number(accl)
 
 # fig = tripartitePLot(periods, PGV)
 # # plt.savefig('bis_spectra.pdf')
